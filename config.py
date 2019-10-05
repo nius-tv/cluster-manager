726608d0bd61f7a6fac446d5cd810ed4bebf0c74
Carlos Chinchilla <calufa@gmail.com>
2019-10-05 11:15:29 -0500

    add config

---
+++ config.py
@@ -0,0 +1,11 @@
+CHECK_TIMEOUT = 2 # in minutes
+CLUSTER_NAME = 'plasmic-generate'
+GPU_TYPE = 'nvidia-tesla-p100'
+INIT_PODS = [
+	'gentle',
+	'init-story'
+]
+MACHINE_TYPE = 'n1-standard-4'
+MAX_CHECKS = 5
+PROJECT_NAME = 'plasmic-artefacts'
+ZONE = 'us-central1-c'
